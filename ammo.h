c7408674ce559fae19038bbee20e87730957fef8
Isaac Valdez <isaacv123456789@gmail.com>
2022-12-15 11:31:23 -0600

    exported multibody classes

--- ammo.h
+++ ammo.h
@@ -4,6 +4,22 @@
 #include "BulletCollision/CollisionShapes/btHeightfieldTerrainShape.h"
 #include "BulletCollision/CollisionShapes/btShapeHull.h"
 #include "BulletDynamics/Character/btKinematicCharacterController.h"
+#include "BulletDynamics/Featherstone/btMultiBody.h"
+#include "BulletDynamics/Featherstone/btMultiBodyLink.h"
+#include "BulletDynamics/Featherstone/btMultiBodyLinkCollider.h"
+#include "BulletDynamics/Featherstone/btMultiBodyConstraint.h"
+#include "BulletDynamics/Featherstone/btMultiBodyJointLimitConstraint.h"
+#include "BulletDynamics/Featherstone/btMultiBodyJointMotor.h"
+#include "BulletDynamics/Featherstone/btMultiBodyPoint2Point.h"
+#include "BulletDynamics/Featherstone/btMultiBodyConstraintSolver.h"
+#include "BulletDynamics/Featherstone/btMultiBodyDynamicsWorld.h"
+#include "BulletDynamics/Featherstone/btMultiBodySolverConstraint.h"
+#include "BulletDynamics/MLCPSolvers/btDantzigLCP.h"
+#include "BulletDynamics/MLCPSolvers/btDantzigSolver.h"
+#include "BulletDynamics/MLCPSolvers/btMLCPSolver.h"
+#include "BulletDynamics/MLCPSolvers/btMLCPSolverInterface.h"
+#include "BulletDynamics/MLCPSolvers/btPATHSolver.h"
+#include "BulletDynamics/MLCPSolvers/btSolveProjectedGaussSeidel.h"
 #include "BulletSoftBody/btDefaultSoftBodySolver.h"
 #include "BulletSoftBody/btSoftBody.h"
 #include "BulletSoftBody/btSoftBodyHelpers.h"
@@ -15,8 +31,11 @@
 //Web IDL doesn't seem to support C++ templates so this is the best we can do
 //https://stackoverflow.com/questions/42517010/is-there-a-way-to-create-webidl-bindings-for-c-templated-types#comment82966925_42517010
 typedef btAlignedObjectArray<btVector3> btVector3Array;
+typedef btAlignedObjectArray<btMatrix3x3> btMatrix3x3Array;
 typedef btAlignedObjectArray<btFace> btFaceArray;
 typedef btAlignedObjectArray<int> btIntArray;
 typedef btAlignedObjectArray<btIndexedMesh> btIndexedMeshArray;
 typedef btAlignedObjectArray<const btCollisionObject*> btConstCollisionObjectArray;
 typedef btAlignedObjectArray<btScalar> btScalarArray;
+
+typedef btAlignedObjectArray<btSolverBody> btSolverBodyArray;
